1ba62f5d5db1b2808579780a82de5d2fa79a917f
Dalton Felipe de Menezes <dalton.menezes@starkbank.com>
2020-04-28 13:56:59 -0300

    Fix event get by id deserialization

--- src/main/java/com/starkbank/utils/Rest.java
+++ src/main/java/com/starkbank/utils/Rest.java
@@ -12,11 +12,16 @@ import java.util.Map;
 
 
 public final class Rest {
+
     public static <T extends Resource> T getId(Resource.ClassData resource, String id, Project user) throws Exception {
         String content = Response.fetch(Api.endpoint(resource, id), "GET", null, null, user).content;
-        JsonObject contentJson = new Gson().fromJson(content, JsonObject.class);
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Event.class, new Event.Deserializer())
+                .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ")
+                .create();
+        JsonObject contentJson = gson.fromJson(content, JsonObject.class);
         JsonObject jsonObject = contentJson.get(Api.getLastName(resource)).getAsJsonObject();
-        return new Gson().fromJson(jsonObject, (Type) resource.cls);
+        return gson.fromJson(jsonObject, (Type) resource.cls);
     }
 
     public static <T extends Resource> List<T> post(Resource.ClassData resource, List<T> entities, Project user) throws Exception {
@@ -48,28 +53,29 @@ public final class Rest {
     public static <T extends Resource> Generator<T> getList(Resource.ClassData resource, Map<String, Object> params, Project user) {
         return new Generator<T>() {
             public void run() throws Exception {
-            Integer limit = (Integer) params.get("limit");
-            String cursor = "";
-            do {
-                params.put("cursor", cursor);
-                if (limit != null){
-                    params.put("limit", limit > 100 ? "100" : limit.toString());
-                    limit -= 100;
-                };
-                String content = Response.fetch(Api.endpoint(resource), "GET", null, params, user).content;
-                Gson gson = new GsonBuilder()
-                        .registerTypeAdapter(Event.class, new Event.Deserializer())
-                        .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ")
-                        .create();
-                JsonObject contentJson = gson.fromJson(content, JsonObject.class);
-                JsonElement cursorJson = contentJson.get("cursor");
-                cursor = cursorJson.isJsonNull() ? "" : cursorJson.getAsString();
-                JsonArray jsonArray = contentJson.get(Api.getLastNamePlural(resource)).getAsJsonArray();
-                for (JsonElement resourceElement : jsonArray) {
-                    JsonObject jsonObject = resourceElement.getAsJsonObject();
-                    yield(gson.fromJson(jsonObject, (Type) resource.cls));
-                }
-            } while(!cursor.isEmpty() && (limit == null || limit > 0));
+                Integer limit = (Integer) params.get("limit");
+                String cursor = "";
+                do {
+                    params.put("cursor", cursor);
+                    if (limit != null) {
+                        params.put("limit", limit > 100 ? "100" : limit.toString());
+                        limit -= 100;
+                    }
+                    ;
+                    String content = Response.fetch(Api.endpoint(resource), "GET", null, params, user).content;
+                    Gson gson = new GsonBuilder()
+                            .registerTypeAdapter(Event.class, new Event.Deserializer())
+                            .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ")
+                            .create();
+                    JsonObject contentJson = gson.fromJson(content, JsonObject.class);
+                    JsonElement cursorJson = contentJson.get("cursor");
+                    cursor = cursorJson.isJsonNull() ? "" : cursorJson.getAsString();
+                    JsonArray jsonArray = contentJson.get(Api.getLastNamePlural(resource)).getAsJsonArray();
+                    for (JsonElement resourceElement : jsonArray) {
+                        JsonObject jsonObject = resourceElement.getAsJsonObject();
+                        yield(gson.fromJson(jsonObject, (Type) resource.cls));
+                    }
+                } while (!cursor.isEmpty() && (limit == null || limit > 0));
             }
         };
     }
