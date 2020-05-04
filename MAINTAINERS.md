861e76a577eefa282bd54d9c6e5ad1f652f8db97
Thales Mello <thalesmello@gmail.com>
2020-05-04 15:30:04 -0700

    Read test credentials from file

--- MAINTAINERS.md
+++ MAINTAINERS.md
@@ -1,3 +1,19 @@
+## Credentials for tests
+
+In order to run tests, set the credentials by creating the file `projectCredentials.json`
+in the root of the project with the following format:
+
+```
+{
+  "environment": "sandbox",
+  "id": "0000000000000000",
+  "privateKey": "-----BEGIN EC PARAMETERS-----00000000000000\n-----END EC PARAMETERS-----\n-----BEGIN EC PRIVATE KEY-----\n0000000000000000000000000000000000000000000000000000000000000000\n00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000\n-----END EC PRIVATE KEY-----\n"
+}
+
+```
+
+The tests will get the credentials in that file by default.
+
 ## Deploying to maven
 
 To upload this file to maven, you have to reference your GPG key and ossrh tokens in your `gradle.properties` file.
--- src/test/java/utils/User.java
+++ src/test/java/utils/User.java
@@ -1,18 +1,21 @@
 package utils;
 
+import java.io.FileReader;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.starkbank.Project;
 
 public class User {
 
     public static Project defaultProject() throws Exception {
-        return new Project(
-            "sandbox",
-            "9999999999999999",
-            "-----BEGIN EC PRIVATE KEY-----\n" +
-                    "MHQCAQEEIBEcEJZLk/DyuXVsEjz0w4vrE7plPXhQxODvcG1Jc0WToAcGBSuBBAAK\n" +
-                    "oUQDQgAE6t4OGx1XYktOzH/7HV6FBukxq0Xs2As6oeN6re1Ttso2fwrh5BJXDq75\n" +
-                    "mSYHeclthCRgU8zl6H1lFQ4BKZ5RCQ==\n" +
-                    "-----END EC PRIVATE KEY-----"
-        );
+        try (FileReader reader = new FileReader("projectCredentials.json")) {
+            JsonObject obj = JsonParser.parseReader(reader).getAsJsonObject();
+            String environment = obj.get("environment").getAsString();
+            String id = obj.get("id").getAsString();
+            String privateKey = obj.get("privateKey").getAsString();
+
+            return new Project(environment, id, privateKey);
+        }
     }
 }
