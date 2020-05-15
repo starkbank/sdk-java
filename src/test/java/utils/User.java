4d4726af359914687c087fb2765d7dafef530b68
Caio Dottori <caio.dottori@starkbank.com>
2020-05-15 20:01:52 -0300

    Refactor test/utils/user to use environment variables instead of hardcoded credentials

--- src/test/java/utils/User.java
+++ src/test/java/utils/User.java
@@ -7,12 +7,8 @@ public class User {
     public static Project defaultProject() throws Exception {
         return new Project(
             "sandbox",
-            "9999999999999999",
-            "-----BEGIN EC PRIVATE KEY-----\n" +
-                    "MHQCAQEEIBEcEJZLk/DyuXVsEjz0w4vrE7plPXhQxODvcG1Jc0WToAcGBSuBBAAK\n" +
-                    "oUQDQgAE6t4OGx1XYktOzH/7HV6FBukxq0Xs2As6oeN6re1Ttso2fwrh5BJXDq75\n" +
-                    "mSYHeclthCRgU8zl6H1lFQ4BKZ5RCQ==\n" +
-                    "-----END EC PRIVATE KEY-----"
+            System.getenv("SANDBOX_ID"), // "9999999999999999",
+            System.getenv("SANDBOX_PRIVATE_KEY") // "-----BEGIN EC PRIVATE KEY-----\nMHQCAQEEIBEcEJZLk/DyuXVsEjz0w4vrE7plPXhQxODvcG1Jc0WToAcGBSuBBAAK\noUQDQgAE6t4OGx1XYktOzH/7HV6FBukxq0Xs2As6oeN6re1Ttso2fwrh5BJXDq75\nmSYHeclthCRgU8zl6H1lFQ4BKZ5RCQ==\n-----END EC PRIVATE KEY-----"
         );
     }
 }
