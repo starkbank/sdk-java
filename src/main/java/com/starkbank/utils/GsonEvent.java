b8f0b5745ed0efe346e5fd11853506f4e1476f6a
Caio Dottori <caio.dottori@starkbank.com>
2020-10-16 18:52:43 -0300

    Add PaymentRequest resource
    Fix null amount on PaymentRequest
    
    Fix payment request

--- src/main/java/com/starkbank/utils/GsonEvent.java
+++ src/main/java/com/starkbank/utils/GsonEvent.java
@@ -3,6 +3,8 @@ package com.starkbank.utils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.starkbank.Event;
+import com.starkbank.PaymentRequest;
+
 
 class GsonEvent {
      private static Gson instance;
@@ -14,6 +16,7 @@ class GsonEvent {
          if(instance == null)
              instance = new GsonBuilder()
                      .registerTypeAdapter(Event.class, new Event.Deserializer())
+                     .registerTypeAdapter(PaymentRequest.class, new PaymentRequest.Deserializer())
                      .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSSZ")
                      .create();
          return instance;
---
+++ src/test/java/TestPaymentRequest.java
@@ -0,0 +1,82 @@
+import com.starkbank.PaymentRequest;
+import com.starkbank.Settings;
+import com.starkbank.Transaction;
+import com.starkbank.utils.Generator;
+import com.starkbank.utils.Resource;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+public class TestPaymentRequest {
+
+    @Test
+    public void testCreate() throws Exception{
+        Settings.user = utils.User.defaultProject();
+        List<PaymentRequest> requests = new ArrayList<>();
+        for(int i=0; i<10; i++) {
+            requests.add(TestPaymentRequest.example());
+        }
+        requests = PaymentRequest.create(requests);
+
+        for(PaymentRequest request : requests) {
+            Assert.assertNotNull(request.id);
+            System.out.println(request);
+        }
+    }
+
+    @Test
+    public void testQuery() throws Exception {
+        Settings.user = utils.User.defaultProject();
+
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("centerId", System.getenv("SANDBOX_CENTER_ID"));
+        params.put("limit", 3);
+        params.put("after", "2019-04-01");
+        params.put("before", "2030-04-30");
+        Generator<PaymentRequest> requests = PaymentRequest.query(params);
+
+        int i = 0;
+        for (PaymentRequest request : requests) {
+            i += 1;
+            System.out.println(request);
+            Assert.assertNotNull(request.id);
+        }
+        System.out.println(i);
+    }
+
+    static PaymentRequest example() throws Exception{
+        Resource payment = TestPaymentRequest.createPayment();
+        String due = null;
+        if(!(payment instanceof Transaction)) {
+            int days = ThreadLocalRandom.current().nextInt(1, 7);
+            LocalDateTime date = LocalDateTime.now().plusDays(days);
+            DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH);
+            due = dateFormat.format(date);
+        }
+        return new PaymentRequest(null, System.getenv("SANDBOX_CENTER_ID"), payment, null, due, null, null, null, null, null, null);
+    }
+
+    static Resource createPayment() throws Exception{
+        int random = ThreadLocalRandom.current().nextInt(0, 4);
+        switch (random) {
+            case 0:
+                return TestTransfer.example(false);
+            case 1:
+                return TestTransaction.example();
+            case 2:
+                return TestBoletoPayment.example(false);
+            case 3:
+                return TestUtilityPayment.example(false);
+            default:
+                throw new Exception("Bad random number");
+        }
+    }
+}
