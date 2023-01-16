64c92dd855820b69d436870a571ffc90652226c4
cdottori <caio.dottori@starkbank.com>
2023-01-16 14:07:39 -0300

    Bump to v2.11.0

--- README.md
+++ README.md
@@ -81,7 +81,7 @@ and add it to your project.
 
 ```sh
 dependencies {
-  implementation 'com.starkbank:sdk:2.10.0'
+  implementation 'com.starkbank:sdk:2.11.0'
 }
 ```
 
@@ -91,7 +91,7 @@ dependencies {
 <dependency>
   <groupId>com.starkbank</groupId>
   <artifactId>sdk</artifactId>
-  <version>2.10.0</version>
+  <version>2.11.0</version>
 </dependency>
 ```
 
--- src/main/java/com/starkbank/utils/Response.java
+++ src/main/java/com/starkbank/utils/Response.java
@@ -129,6 +129,6 @@ public final class Response {
     }
 
     private static String getUserAgent() {
-        return (userAgentOverride == null) ? "Java-" + System.getProperty("java.version") + "-SDK-2.10.0" : userAgentOverride;
+        return (userAgentOverride == null) ? "Java-" + System.getProperty("java.version") + "-SDK-2.11.0" : userAgentOverride;
     }
 }
